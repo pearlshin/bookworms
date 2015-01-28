876ed98ee13df099d4559bf13486c0754a9038e9
Pearl Shin <pearlshin22@gmail.com>
2015-01-28 12:02:15 -0500

    add summary field to books

--- db/migrate/20150128093015_create_books.rb
+++ db/migrate/20150128093015_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :title
       t.string :author
+      t.text :summary
       t.references :user
 
       t.timestamps
