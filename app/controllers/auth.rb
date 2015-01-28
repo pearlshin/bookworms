bc4b37c2839ab113464c2abe4ab54e37d447632c
Pearl Shin <pearlshin22@gmail.com>
2015-01-28 12:01:02 -0500

    add initial auth controller routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,41 @@
 get '/' do
-  erb :welcome
+  if current_user
+    erb :index
+  end
+  erb :index
 end
+
+
 
 get '/login' do
-  
+  erb :'auth/login'
 end
 
+post '/login' do
+  user = User.find_by(username: params[:user][:username])
+  user.try(:authenticate, params[:user][:password])
+  session[:user_id] = user.id
+  if request.xhr?
+    redirect '/'
+  else
+    # set_error('uh oh!')
+    redirect '/'
+  end
+end
+
+
 get '/signup' do
-  
+
+end
+
+
+get '/logout' do
+  session.clear
+  redirect "/"
+end
+
+
+get '/user/:id' do
+  @user = User.find(params[:id])
+  erb :'auth/user_profile'
 end
