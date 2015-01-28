8ca6e11cb11d639f0e6b1019c61467a6aef71f27
Pearl Shin <pearlshin22@gmail.com>
2015-01-28 10:11:20 -0500

    initial model and migration tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # Remember to create a migration!
+  has_many :books, through: :user_books
+  has_many :user_books
+  has_many :reviews
+
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
