cb0565027050205084af3b11a2602932d421d85c
Pearl Shin <pearlshin22@gmail.com>
2015-01-28 12:03:16 -0500

    add first ajax call for login form

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,29 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $(".home-login").on("click", function(event) {
+    event.preventDefault();
+    // console.log("it's working!");
+    $(".login").toggle();
+  });
+
+  $("form.login-submit").on("submit", function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+    var url = $target.attr("action");
+    var type = $target.attr("method");
+    var data = $target.serialize();
+    $.ajax ({
+      url: url,
+      type: type,
+      data: data
+    }).done(function(response) {
+      // console.log(response);
+      $target[0].reset();
+      window.location.href="/";
+    });
+  });
+
+
 });
